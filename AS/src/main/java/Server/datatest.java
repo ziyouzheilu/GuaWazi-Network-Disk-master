52f66b311042094a80e60e30070d80fbe1b4d911
huangsheng <2334170767@qq.com>
2022-05-25 16:18:35 +0800

    second

--- AS/src/main/java/Server/datatest.java
+++ AS/src/main/java/Server/datatest.java
@@ -19,7 +19,8 @@ public class datatest {
     public static void main(String[] args) {
         Connection conn = null;
         Statement stmt = null;
-        System.out.println("nihao");
+        System.out.println("a");
+        System.out.println("第二次修改");
         try{
             // 注册 JDBC 驱动
             Class.forName(JDBC_DRIVER);
